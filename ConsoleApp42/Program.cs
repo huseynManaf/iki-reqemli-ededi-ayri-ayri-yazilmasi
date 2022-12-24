b043fd056a396f43e704e27cbbddd2cf6cae3c57
huseynManaf <huseynim@code.edu.az>
2022-12-24 05:06:04 -0800

    init

---
+++ ConsoleApp42/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp42
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iki reqemli eded daxil edin");
+            string input = Console.ReadLine();
+            if (input.Length < 2 || input.Length > 2) 
+            {
+                Console.WriteLine("Eded iki reqemli deyil");
+                return;
+            }
+            int number = int.Parse(input);
+            int LastDigit = number % 10;
+            number /= 10;
+            Console.WriteLine($"{ number} {LastDigit}");
+
+
+        }
+    }
+}
